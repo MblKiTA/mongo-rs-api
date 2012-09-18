250632c4339ee4c596a8a1b4094ed55fdea51d3e
Nikita Basalaev <nikita@mail.by>
2012-09-18 13:26:08 +0300

    Now RSs could be started and stopped concurrenlty.

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import sys
-sys.path.insert(0, 'mongo-python-driver/test/high_availability')
+import sys, os, time
 
+# Import pymongo
+sys.path.insert(0, 'mongo-python-driver')
+from pymongo import ReplicaSetConnection
+
+# Import HA Tools
+sys.path.insert(0, 'mongo-python-driver/test/high_availability')
 import ha_tools
 
 import tornado.ioloop
@@ -9,6 +14,15 @@ import tornado.template as template
 
 import uuid
 import json
+
+new_repl_id = -1
+
+def get_new_repl_id():
+    global new_repl_id
+    new_repl_id += 1
+    return new_repl_id
+
+rs = []
 
 class RsHandler(tornado.web.RequestHandler):
     # Reply's tpls extension
@@ -31,6 +45,9 @@ class RsHandler(tornado.web.RequestHandler):
 
         return json_data
 
+    def _get_index(self, seq, attribute, value):
+        return next(index for (index, d) in enumerate(seq) if d[attribute] == value)
+
     def message(self, message):
         self.write(self._template.load('message' + self._ext).generate(message=message))
 
@@ -42,33 +59,74 @@ class RsHandler(tornado.web.RequestHandler):
     def post(self, op):
         """ Respond to a POST
         """
-
         # Start RS
         if op == 'start':
             request = self._parse_json(self.request.body)
             members = request['members']
 
+            rs_id = uuid.uuid4()
+            rs_name = 'repl' + str(get_new_repl_id())
+
             try:
                 # start RS and get its' params
-                res = ha_tools.start_replica_set(members)
+                res = ha_tools.start_replica_set(members, rs_name=rs_name)
             except:
                 raise tornado.httpserver._BadRequestException(
                     "Couldn't start RS!"
                 )
 
-            rs_uri, rs_name = res
-            rs_id = uuid.uuid4()
+            # Let's gather all the info about RS and save it
+            rs_uri, rs_name, nodes = res
+
+            # Try to connect to new RS
+            try:
+                for _ in range(1 * 60 / 2):
+                    time.sleep(2)
+
+                    try:
+                        c = ReplicaSetConnection(rs_uri, replicaSet=rs_name)
+                    except:
+                        pass
+            except:
+                raise tornado.httpserver._BadRequestException(
+                    "Couldn't connect to the new RS: " + rs_uri + ", " + rs_name
+                )
+
+            secondaries_uris = []
+
+            for secondary in c.secondaries:
+                secondaries_uris.append('%s:%d' % (secondary))
+
+            rs_t = {}
+
+            rs_t['id'] = str(rs_id)
+            rs_t['name'] = rs_name
+            rs_t['primary'] = rs_uri
+            rs_t['secondaries'] = secondaries_uris
+            rs_t['nodes'] = nodes
+
+            rs.append(rs_t)
 
             self.write(self._template.load(op + self._ext).generate(rs_id=rs_id, rs_uri=rs_uri, rs_name=rs_name))
 
         # Stop rs
         elif op == 'stop':
+            request = self._parse_json(self.request.body)
+            rs_id = request['rs']['id']
+
+            found_index = self._get_index(rs, 'id', rs_id)
+
             try:
-                ha_tools.kill_all_members()
+                ha_tools.kill_members(rs[found_index]['nodes'].keys(), 2, rs[found_index]['nodes'])
+                rs_t = rs.pop(found_index)
             except:
                 raise tornado.httpserver._BadRequestException(
                     "Couldn't stop RS!"
                 )
+
+            rs_id = rs_t['id']
+
+            self.write(self._template.load(op + self._ext).generate(rs_id=rs_id))
 
         # Get primary
         elif op == 'get_primary':
@@ -123,6 +181,8 @@ class RsHandler(tornado.web.RequestHandler):
             rs_killed_secondaries_uris = ha_tools.kill_all_secondaries()
 
             self.write(self._template.load(op + self._ext).generate(rs_id=rs_id, rs_killed_secondaries_uris=rs_killed_secondaries_uris))
+
+
 
 application = tornado.web.Application([
     (r"/rs/([_a-z]*)", RsHandler)
